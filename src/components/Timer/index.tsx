7ffa76df18c95789f0fbb9b4a411e018c8c6725b
markoclimako <mkclimako@gmail.com>
2023-07-15 13:35:43 -0300

    chore: Reset application and start development from scratch

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,49 +1,5 @@
 'use client'
-import { TimerProps } from '@/types'
-import { TimerControls } from './TimerControls'
-import { TimerDisplay } from './TimerDisplay'
-import { TimerFormModal } from './TimerFormModal'
 
-export function Timer({
-  handleCloseModal,
-  handleOpenModal,
-  handlePause,
-  handleSetTimerValue,
-  handleStart,
-  handleStop,
-  isCountdownActive,
-  minutesRemaining,
-  modalIsOpen,
-  onClick,
-  secondsRemaining,
-  selectedFeature,
-  timeInputPlaceholder,
-  timeInputValue,
-}: TimerProps) {
-  return (
-    <>
-      <section className="flex flex-col items-center rounded-lg bg-carbon_100 px-16 py-4 text-center md:px-16">
-        <TimerDisplay
-          minutesRemaining={minutesRemaining}
-          secondsRemaining={secondsRemaining}
-        />
-        <TimerControls
-          handleOpenModal={handleOpenModal}
-          handleStop={handleStop}
-          handleStart={handleStart}
-          handlePause={handlePause}
-          isCountdownActive={isCountdownActive}
-          selectedFeature={selectedFeature}
-        />
-      </section>
-      <TimerFormModal
-        modalIsOpen={modalIsOpen}
-        handleCloseModal={handleCloseModal}
-        handleSetTimerValue={handleSetTimerValue}
-        onClick={onClick}
-        timeInputPlaceholder={timeInputPlaceholder}
-        timeInputValue={timeInputValue}
-      />
-    </>
-  )
+export function Timer() {
+  return <></>
 }
